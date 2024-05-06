6cf56339407798bde2c503499eafaeb0cb39be69
Tom Tom <pkiet8622@gmail.com>
2024-05-07 04:26:09 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,10 @@
+
+const nameGirl = 'Bạn';
+const giftUrl = 'http://https://www.facebook.com/n.leen1209';
+const eventName = 'Mở Ra';
+const titleCard = 'Mở Ra Đi';
+const contentCard = 'Ngủ Ngon Và Ngủ Sớm Đi Và Nói Nhỏ Lại Xíu Nhé';
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
